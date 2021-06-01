2ad7891ceb5c9e0d54950c44dbc5aaa01ebdcbf1
Joe Fredette <jfredett@gmail.com>
2021-05-31 23:23:13 -0400

    [bitboards] fix documentation

--- src/bitboard/mod.rs
+++ src/bitboard/mod.rs
@@ -35,11 +35,14 @@
 /// 6| 50 51 52 53 54 55 56 57
 /// 5| 40 41 42 43 44 45 46 47
 /// 4| 30 31 32 33 34 35 36 37
-/// 3| 20 21 22 23 24 25 26 27 
+/// 3| 20 21 22 23 24 25 26 27
 /// 2| 10 11 12 13 14 15 16 17
-/// 1| 0  1  2  3  4  5  6  7
+/// 1| 00 01 02 03 04 05 06 07
 ///  ------------------------
 ///    a  b  c  d  e  f  g  h
+///
+/// Octal is the most handy representation for understanding where things are located, but the
+/// literature usually uses decimal.
 #[derive(Hash, PartialEq, Eq, Clone, Copy)]
 pub struct Bitboard(u64);
 
