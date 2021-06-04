86ab4bcca9ee84f4b1bcb988b9c1bb3098704a01
Joe Fredette <jfredett@gmail.com>
2021-06-04 13:32:17 -0400

    [ply] add constants::masks

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -2,8 +2,10 @@ pub mod file;
 pub mod conversion_tables;
 pub mod color;
 pub mod piece;
+pub mod masks;
 
 pub use conversion_tables::*;
 pub use file::*;
 pub use color::*;
 pub use piece::*;
+pub use masks::*;
