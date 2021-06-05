59e1d0155e5f54e57be8db84c4f513426db4c50c
Joe Fredette <jfredett@gmail.com>
2021-06-05 13:09:59 -0400

    [ply] implement Ply#{occupancy/0, occupancy_for/1}

--- src/ply/mod.rs
+++ src/ply/mod.rs
@@ -39,6 +39,44 @@ pub struct Ply {
 
 // parse a fen string and construct the ply
 impl Ply {
+    /// Provides a bitboard which shows the location of all squares occupied by pieces of the given
+    /// color
+    /// ```
+    /// # #[macro_use] extern crate hazel;
+    /// # use hazel::bitboard::Bitboard;
+    /// # use hazel::ply::Ply;
+    /// # use hazel::constants::*;
+    /// let fen = "8/5k1p/2n5/3N4/6P1/3K4/8/8 w - - 0 1".to_string();
+    /// let ply = Ply::from_fen(&fen);
+    /// let expected_occupancy_white = bitboard!("d3", "d5", "g4");
+    /// let expected_occupancy_black = bitboard!("c6", "f7", "h7");
+    /// assert_eq!(ply.occupancy_for(Color::WHITE), expected_occupancy_white);
+    /// assert_eq!(ply.occupancy_for(Color::BLACK), expected_occupancy_black);
+    /// ```
+    pub fn occupancy_for(&self, color: Color) -> Bitboard {
+        self.kings[color as usize]   |
+        self.queens[color as usize]  |
+        self.rooks[color as usize]   |
+        self.bishops[color as usize] |
+        self.knights[color as usize] |
+        self.pawns[color as usize]
+    }
+    
+    /// Provides a bitboard which shows the location of all squares occupied by pieces of any
+    /// color
+    /// ```
+    /// # #[macro_use] extern crate hazel;
+    /// # use hazel::bitboard::Bitboard;
+    /// # use hazel::ply::Ply;
+    /// # use hazel::constants::*;
+    /// let fen = "8/5k1p/2n5/3N4/6P1/3K4/8/8 w - - 0 1".to_string();
+    /// let ply = Ply::from_fen(&fen);
+    /// let expected_occupancy_white = bitboard!("d3", "d5", "g4");
+    /// let expected_occupancy_black = bitboard!("c6", "f7", "h7");
+    /// assert_eq!(ply.occupancy(), expected_occupancy_white | expected_occupancy_black);
+    /// ```
+    pub fn occupancy(&self) -> Bitboard {
+        self.occupancy_for(Color::WHITE) | self.occupancy_for(Color::BLACK)
     }
 
     pub fn piece_at(&self, file: File, rank: usize, piece: Piece, color: Color) -> bool {
