5e4b02c40f22eee247303fc912423bb568da1156
Joe Fredette <jfredett@gmail.com>
2021-06-07 10:41:02 -0400

    [ply-movegen] Calculate knight moves via lookup table
    _so_ much better than doing this inline. Pawn moves next, then it's
    sliding pieces and magic bitboards

--- src/constants/test.rs
+++ src/constants/test.rs
@@ -10,6 +10,8 @@ pub const START_POSITION_FEN : &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKB
 pub const LONDON_POSITION_FEN : &str = "r1bqk2r/pp2bppp/2n1pn2/2pp4/3P1B2/2P1PN1P/PP1N1PP1/R2QKB1R b KQkq - 0 7";
 /// FEN which includes an en passant move
 pub const EN_PASSANT_POSITION_FEN : &str = "r1bqk2r/pp2bppp/2n1pn2/3p4/1PpP1B2/2P1PN1P/P2N1PP1/R2QKB1R b KQkq b3 0 8";
+/// FEN for a position what starts after white plays 1. d4
+pub const D4_POSITION_FEN : &str = "rnbqkbnr/pppppppp/8/8/8/3P4/PPP1PPPP/RNBQKBNR b KQkq - 0 1";
 
 
 lazy_static! {
@@ -35,5 +37,28 @@ lazy_static! {
         Move::from_notation("b1", "c3", Either::Left(MoveType::quiet())),
         Move::from_notation("g1", "f3", Either::Left(MoveType::quiet())),
         Move::from_notation("g1", "h3", Either::Left(MoveType::quiet())),
+    ];
+
+    pub static ref D4_MOVES : Vec<Move> = vec![
+        Move::from_notation("a7", "a5", Either::Left(MoveType::quiet())),
+        Move::from_notation("a7", "a6", Either::Left(MoveType::quiet())),
+        Move::from_notation("b7", "b5", Either::Left(MoveType::quiet())),
+        Move::from_notation("b7", "b6", Either::Left(MoveType::quiet())),
+        Move::from_notation("c7", "c5", Either::Left(MoveType::quiet())),
+        Move::from_notation("c7", "c6", Either::Left(MoveType::quiet())),
+        Move::from_notation("d7", "d5", Either::Left(MoveType::quiet())),
+        Move::from_notation("d7", "d6", Either::Left(MoveType::quiet())),
+        Move::from_notation("e7", "e5", Either::Left(MoveType::quiet())),
+        Move::from_notation("e7", "e6", Either::Left(MoveType::quiet())),
+        Move::from_notation("f7", "f5", Either::Left(MoveType::quiet())),
+        Move::from_notation("f7", "f5", Either::Left(MoveType::quiet())),
+        Move::from_notation("g7", "g5", Either::Left(MoveType::quiet())),
+        Move::from_notation("g7", "g5", Either::Left(MoveType::quiet())),
+        Move::from_notation("h7", "h6", Either::Left(MoveType::quiet())),
+        Move::from_notation("h7", "h6", Either::Left(MoveType::quiet())),
+        Move::from_notation("b8", "a6", Either::Left(MoveType::quiet())),
+        Move::from_notation("b8", "c6", Either::Left(MoveType::quiet())),
+        Move::from_notation("g8", "f6", Either::Left(MoveType::quiet())),
+        Move::from_notation("g8", "h6", Either::Left(MoveType::quiet())),
     ];
 }
--- src/movement/generator.rs
+++ src/movement/generator.rs
@@ -1,9 +1,39 @@
 use super::*;
 use crate::{
-    bitboard::{Direction},
-    constants::{Color, FILE_MASKS, RANK_MASKS, RANK_1, RANK_2, RANK_7, RANK_8}, 
+    bitboard::{Bitboard, Direction},
+    constants::*,
     ply::*
 };
+
+
+pub fn calculate_knight_moves() -> [Bitboard; 64] {
+    let mut out : [Bitboard; 64] = [Bitboard::empty(); 64];
+    for i in 0..64 {
+            let mut bb = Bitboard::empty();
+            bb.set_by_index(i);
+            
+            let position_board = bb.shift(Direction::N).shift(Direction::N).shift(Direction::E) // NNE
+                               | bb.shift(Direction::N).shift(Direction::N).shift(Direction::W) // NNW
+                               | bb.shift(Direction::W).shift(Direction::W).shift(Direction::N) // WWN
+                               | bb.shift(Direction::W).shift(Direction::W).shift(Direction::S) // WWS
+                               | bb.shift(Direction::S).shift(Direction::S).shift(Direction::W) // SSW
+                               | bb.shift(Direction::S).shift(Direction::S).shift(Direction::E) // SSE
+                               | bb.shift(Direction::E).shift(Direction::E).shift(Direction::S) // EES
+                               | bb.shift(Direction::E).shift(Direction::E).shift(Direction::N) // EEN
+                               ;
+            out[i] = position_board;
+    }
+    out
+}
+
+lazy_static! {
+    /// A lookup table to convert a knight on idx `n` -> it's bitboard of attacks/moves
+    pub static ref KNIGHT_MOVES: [Bitboard; 64] = calculate_knight_moves();
+
+    
+}
+
+
 
 impl Move {
     /// Generates all valid moves from the given ply.
@@ -12,13 +42,17 @@ impl Move {
     pub fn generate(&ply : &Ply, color: Color) -> Vec<Move> {
         let mut out : Vec<Move> = vec![];
         let occupancy = ply.occupancy();
+        let pawn_direction = match color {
+            Color::WHITE => Direction::N,
+            Color::BLACK => Direction::S
+        };
         // pawn moves
         for &file in FILE_MASKS.iter() {
             for &rank in RANK_MASKS.iter() {
                 let pawn = ply.pawns[color as usize] & file & rank;
                 if pawn.is_empty() { continue; }
-                if (occupancy & pawn.shift(Direction::N)).is_empty() {
-                    let shifted = pawn.shift(Direction::N);
+                if (occupancy & pawn.shift(pawn_direction)).is_empty() {
+                    let shifted = pawn.shift(pawn_direction);
                     let source_idx = pawn.all_set_indices()[0];
                     let target_idx = shifted.all_set_indices()[0];
                     if rank == *RANK_8 || rank == *RANK_1 {
@@ -34,10 +68,10 @@ impl Move {
                 }
 
                 if rank == *RANK_2 || rank == *RANK_7 {
-                    if (occupancy & pawn.shift(Direction::N).shift(Direction::N)).is_empty() {
+                    if (occupancy & pawn.shift(pawn_direction).shift(pawn_direction)).is_empty() {
                         // TODO: maybe a bb_shift! macro for shifting by arbitrary amounts?
                         // TODO: extract the source_idx/target_idx stuff into a helper
-                        let shifted = pawn.shift(Direction::N).shift(Direction::N);
+                        let shifted = pawn.shift(pawn_direction).shift(pawn_direction);
                         let source_idx = pawn.all_set_indices()[0];
                         let target_idx = shifted.all_set_indices()[0];
                         // TODO: Do attack/check/capture checking
@@ -49,27 +83,10 @@ impl Move {
         // king moves
         // knight moves
         let knights = ply.knights[color as usize];
-        for &file in FILE_MASKS.iter() {
-            for &rank in RANK_MASKS.iter() {
-                let k = knights & file & rank;
-                if k.is_empty() { continue; }
-
-                let source_idx = k.all_set_indices()[0];
-                let k_nne = k.shift(Direction::N).shift(Direction::N).shift(Direction::E);
-                // if it's not off-the-board, and it's not blocked by our guys, then we add it
-                if !k_nne.is_empty() && (k_nne & ply.occupancy_for(color)).is_empty() {
-                    let target_idx = k_nne.all_set_indices()[0];
-                    // TODO: attack/check/capture checking
-                    out.push(Move::from(source_idx as u16, target_idx as u16, false, 0b000)) 
-                }
-
-                let k_nnw = k.shift(Direction::N).shift(Direction::N).shift(Direction::W);
-                // if it's not off-the-board, and it's not blocked by our guys, then we add it
-                if !k_nnw.is_empty() && (k_nnw & ply.occupancy_for(color)).is_empty() {
-                    let target_idx = k_nnw.all_set_indices()[0];
-                    // TODO: attack/check/capture checking
-                    out.push(Move::from(source_idx as u16, target_idx as u16, false, 0b000)) 
-                }
+        for k in knights.all_set_indices() {
+            let unblocked_squares = KNIGHT_MOVES[k] & !ply.occupancy_for(color);
+            for square in unblocked_squares.all_set_indices() {
+                out.push(Move::from(k as u16, square as u16, false, 0b000));
             }
         }
         // rook moves
@@ -82,16 +99,24 @@ impl Move {
 
 #[cfg(test)]
 mod test {
-    use crate::constants::{START_POSITION_FEN, STARTING_MOVES};
-
+    use crate::constants::*;
     use super::*;
-    
 
     #[test]
     fn calculates_starting_position_moves() {
         let ply = Ply::from_fen(&String::from(START_POSITION_FEN));
         let moves = Move::generate(&ply, Color::WHITE);
         for m in STARTING_MOVES.iter() {
+            if !moves.contains(&m) { dbg!("missing move", m); }
+            assert!(moves.contains(&m));
+        }
+    }
+    
+    #[test]
+    fn calculates_move_after_1_d4_correctly() {
+        let ply = Ply::from_fen(&String::from(D4_POSITION_FEN));
+        let moves = Move::generate(&ply, Color::BLACK);
+        for m in D4_MOVES.iter() {
             if !moves.contains(&m) { dbg!("missing move", m); }
             assert!(moves.contains(&m));
         }
