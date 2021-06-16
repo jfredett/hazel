ee1363202e7c1c21c9a5fed076c7a061c273608a
Joe Fredette <jfredett@gmail.com>
2021-06-16 14:43:05 -0400

    [ply-movegen] king moves, pawn move refactor

--- src/movement/generator.rs
+++ src/movement/generator.rs
@@ -1,10 +1,9 @@
 use super::*;
 use crate::{
-    bitboard::{Bitboard, Direction},
+    bitboard::Bitboard,
     constants::*,
     ply::*
 };
-
 
 pub fn calculate_knight_moves() -> [Bitboard; 64] {
     let mut out : [Bitboard; 64] = [Bitboard::empty(); 64];
@@ -26,14 +25,65 @@ pub fn calculate_knight_moves() -> [Bitboard; 64] {
     out
 }
 
-lazy_static! {
-    /// A lookup table to convert a knight on idx `n` -> it's bitboard of attacks/moves
-    pub static ref KNIGHT_MOVES: [Bitboard; 64] = calculate_knight_moves();
+pub fn calculate_pawn_moves() -> [[Bitboard; 64]; 2] {
+    let mut white_out = [Bitboard::empty(); 64];
+    let mut black_out = [Bitboard::empty(); 64];
+    // pawn moves, initial rank
+    for i in 8..17 {
+        let mut wbb = Bitboard::empty();
+        wbb.set_by_index(i);
+        let mut bbb = Bitboard::empty();
+        bbb.set_by_index(64-i);
 
-    
+        wbb |= wbb.shift(Direction::N)
+            |  wbb.shift(Direction::N).shift(Direction::N)
+            |  wbb.shift(Direction::NE)
+            |  wbb.shift(Direction::NW);
+            
+        bbb |= bbb.shift(Direction::S)
+            |  bbb.shift(Direction::S).shift(Direction::S)
+            |  bbb.shift(Direction::SE)
+            |  bbb.shift(Direction::SW);
+
+
+        white_out[i] = wbb;
+        black_out[64-i] = bbb;
+    }
+
+    // all other pawn moves
+    for i in 17..64 {
+        let mut wbb = Bitboard::empty();
+        wbb.set_by_index(i);
+        let mut bbb = Bitboard::empty();
+        bbb.set_by_index(64-i);
+
+        wbb |= wbb.shift(Direction::N)
+            |  wbb.shift(Direction::NE)
+            |  wbb.shift(Direction::NW);
+            
+        bbb |= bbb.shift(Direction::S)
+            |  bbb.shift(Direction::SE)
+            |  bbb.shift(Direction::SW);
+
+        white_out[i] = wbb;
+        black_out[64-i] = bbb;
+    }
+
+
+    [ white_out, black_out ]
 }
 
+lazy_static! {
+    // NOTE: for all of these we only care about specifying what squares _could_ be moved to, not
+    // if that move would be legal or not, we leave legality analysis for the generator
+    // That analysis would look something like -- mask off areas based on the opposing color, if they're 
+    // 
 
+    /// A lookup table to convert a knight on a given index -> it's bitboard of moves
+    pub static ref KNIGHT_MOVES: [Bitboard; 64] = calculate_knight_moves();
+    /// A lookup table to convert a pawn on a given index -> it's bitboard of moves
+    pub static ref PAWN_MOVES: [[Bitboard; 64]; 2] = calculate_pawn_moves();
+}
 
 impl Move {
     /// Generates all valid moves from the given ply.
@@ -41,46 +91,52 @@ impl Move {
     ///     Future versions will be refactored from this to build a faster algorithm.
     pub fn generate(&ply : &Ply, color: Color) -> Vec<Move> {
         let mut out : Vec<Move> = vec![];
-        let occupancy = ply.occupancy();
-        let pawn_direction = match color {
-            Color::WHITE => Direction::N,
-            Color::BLACK => Direction::S
+        let other_color = match color {
+            Color::WHITE => Color::BLACK,
+            Color::BLACK => Color::WHITE
         };
-        // pawn moves
-        for &file in FILE_MASKS.iter() {
-            for &rank in RANK_MASKS.iter() {
-                let pawn = ply.pawns[color as usize] & file & rank;
-                if pawn.is_empty() { continue; }
-                if (occupancy & pawn.shift(pawn_direction)).is_empty() {
-                    let shifted = pawn.shift(pawn_direction);
-                    let source_idx = pawn.all_set_indices()[0];
-                    let target_idx = shifted.all_set_indices()[0];
-                    if rank == *RANK_8 || rank == *RANK_1 {
-                        // we can promote to any piece we like.
-                        out.push(Move::from(source_idx as u16, target_idx as u16, true, 0b00));
-                        out.push(Move::from(source_idx as u16, target_idx as u16, true, 0b01));
-                        out.push(Move::from(source_idx as u16, target_idx as u16, true, 0b10));
-                        out.push(Move::from(source_idx as u16, target_idx as u16, true, 0b11));
-                    } else {
-                        // TODO: Do attack/check/capture checking
-                        out.push(Move::from(source_idx as u16, target_idx as u16, false, 0b000));
-                    }
-                }
 
-                if rank == *RANK_2 || rank == *RANK_7 {
-                    if (occupancy & pawn.shift(pawn_direction).shift(pawn_direction)).is_empty() {
-                        // TODO: maybe a bb_shift! macro for shifting by arbitrary amounts?
-                        // TODO: extract the source_idx/target_idx stuff into a helper
-                        let shifted = pawn.shift(pawn_direction).shift(pawn_direction);
-                        let source_idx = pawn.all_set_indices()[0];
-                        let target_idx = shifted.all_set_indices()[0];
-                        // TODO: Do attack/check/capture checking
-                        out.push(Move::from(source_idx as u16, target_idx as u16, false, 0b000));
+        // pawn moves
+        for source in ply.pawns[color as usize].all_set_indices() {
+            let target_board = PAWN_MOVES[color as usize][source];
+            for target in target_board.all_set_indices() {
+                // if it's a promotion, push the promotion moves
+                if target >= 56 || target <= 8 { // on the first or last rank
+                    out.push(Move::from(source as u16, target as u16, true, 0b00));
+                    out.push(Move::from(source as u16, target as u16, true, 0b01));
+                    out.push(Move::from(source as u16, target as u16, true, 0b10));
+                    out.push(Move::from(source as u16, target as u16, true, 0b11));
+                } 
+                
+                // the bottom 3 bits of an index determine it's file.
+                if (source & 0b0111) == (target & 0b0111) { // advances
+                    if !(ply.occupancy_for(color) & target_board).is_empty() {
+                        if !ply.occupancy_for(color).all_set_indices().contains(&target) { 
+                            out.push(Move::from(source as u16, target as u16, false, 0b000));
+                        }
+                    } 
+                } else { // attacks
+                    if !(ply.occupancy_for(other_color) & target_board).is_empty() {
+                        if ply.occupancy_for(other_color).all_set_indices().contains(&target) { 
+                            out.push(Move::from(source as u16, target as u16, false, 0b000));
+                        }
                     }
                 }
             }
         }
         // king moves
+        // FIXME: Doesn't account for checks yet.
+        let king = ply.kings[color as usize];
+        let source = king.all_set_indices()[0];
+        for d in DIRECTIONS {
+            let m = king.shift(d);
+            if m.is_empty() { continue; }
+
+            if (m & ply.occupancy_for(color)).is_empty() {
+                let target = m.all_set_indices()[0];
+                out.push(Move::from(source as u16, target as u16, false, 0b000));
+            }
+        }
         // knight moves
         let knights = ply.knights[color as usize];
         for k in knights.all_set_indices() {
@@ -101,6 +157,10 @@ impl Move {
 mod test {
     use crate::constants::*;
     use super::*;
+    
+
+    // TODO: Have a yaml file which describes a bunch of test positions and the valid moves they entail, load them, then generate tests 
+    // from them, we can do this by taking random positions from a database, using stockfish to perft 1 them, then grab the results.
 
     #[test]
     fn calculates_starting_position_moves() {
@@ -121,4 +181,22 @@ mod test {
             assert!(moves.contains(&m));
         }
     }
+    
+    #[test]
+    fn calculates_correct_movecount_kiwipete() {
+        let ply = Ply::from_fen(&String::from(POS2_KIWIPETE_FEN));
+        let moves = Move::generate(&ply, ply.current_player());
+        assert_eq!(moves.len(), POS2_KIWIPETE_PERFT_COUNTS[0]);
+    }
+    
+    #[test]
+    fn calculates_moves_for_kiwipete_position_at_depth_1() {
+        let ply = Ply::from_fen(&String::from(POS2_KIWIPETE_FEN));
+        let moves = Move::generate(&ply, ply.current_player());
+        for m in POS2_KIWIPETE_MOVES.iter() {
+            if !moves.contains(&m) { dbg!("missing move", m); }
+            assert!(moves.contains(&m));
+        }
+    }
+    
 }
