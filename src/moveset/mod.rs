47d4ea6193a7c64b60f23418ea2dded9ce0024ef
Joe Fredette <jfredett@gmail.com>
2021-06-16 13:42:39 -0400

    [ply-movegen] add remaining add_move methods

--- src/moveset/mod.rs
+++ src/moveset/mod.rs
@@ -1,4 +1,4 @@
-use crate::{constants::Piece, movement::Move};
+use crate::{constants::Piece, movement::{Move, MoveType}};
 
 
 /// a container for moves
@@ -10,6 +10,22 @@ pub struct MoveList {
 impl MoveList {
     pub fn empty() -> MoveList {
         return MoveList { moves: vec![] };
+    }
+    
+    pub fn add_move(&mut self, source: usize, target: usize) {
+        self.moves.push(Move::from(source as u16, target as u16, false, MoveType::quiet().bits()));
+    }
+    
+    pub fn add_capture(&mut self, source: usize, target: usize) {
+        self.moves.push(Move::from(source as u16, target as u16, false, MoveType::capture().bits()));
+    }
+
+    pub fn add_check(&mut self, source: usize, target: usize) {
+        self.moves.push(Move::from(source as u16, target as u16, false, MoveType::check().bits()));
+    }
+
+    pub fn add_attack(&mut self, source: usize, target: usize) {
+        self.moves.push(Move::from(source as u16, target as u16, false, MoveType::attack().bits()));
     }
     
     pub fn add_promotion(&mut self, source: usize, target: usize) {
@@ -25,12 +41,36 @@ impl MoveList {
 mod test {
     use either::Either;
 
-    use crate::{constants::Piece, movement::Move};
+    use crate::{constants::{NOTATION_TO_INDEX, Piece}, movement::Move};
 
     use super::*;
     
+    /// passes if the left is a subset of the right
+    macro_rules! assert_is_subset {
+        ($left:expr, $right:expr) => (
+            let mut missing = vec![];
+            for m in $left {
+               if !$right.contains(&m) {
+                    missing.push(m);
+               } 
+            } 
+            
+            if missing.len() > 0 {
+                panic!("assertion failed, set difference: {:?}", missing);
+            }
+        );
+    }
+    
+    /// This is essentially assert_eq but doesn't care about order differences
+    macro_rules! assert_are_equal_sets {
+        ($left:expr, $right:expr) => (
+            assert_is_subset!(&$left, &$right);
+            assert_is_subset!(&$right, &$left);
+        );
+    }
+    
     #[test]
-    fn add_promotion_adds_promtion_moves() {
+    fn add_promotion_adds_promtion_moves_refactor() {
         let mut ml = MoveList::empty();
         // h7->h8
         ml.add_promotion(56, 64);
@@ -41,26 +81,62 @@ mod test {
             Move::from(56, 64, true, Piece::Bishop as u16),
             Move::from(56, 64, true, Piece::Knight as u16)
         ];
-        
-        let mut should_be_present_but_missing = vec![];
-        let mut should_be_missing_but_present = vec![];
 
-        let moves = ml.moves.clone();
+        assert_are_equal_sets!(ml.moves, expected);
+    }
+    
+    #[test]
+    fn add_capture_adds_capture() {
+        let mut ml = MoveList::empty();
+        let expected_move = Move::from_notation("b4", "d5", Either::Left(MoveType::CAPTURE));
 
-        for m in ml.moves {
-            if !expected.contains(&m) {
-                should_be_missing_but_present.push(m);
-            }
-        }
+        ml.add_capture(
+            NOTATION_TO_INDEX("b4"),
+            NOTATION_TO_INDEX("d5")
+        );
         
-        for m in expected {
-            if !moves.contains(&m) {
-                should_be_present_but_missing.push(m);
-            }
-        }
-        
-        assert_eq!(should_be_present_but_missing, vec![]);
-        assert_eq!(should_be_missing_but_present, vec![]);
+        assert_eq!(ml.moves.len(), 1);
+        assert!(ml.moves.contains(&expected_move));
+    }
 
+    #[test]
+    fn add_move_adds_move() {
+        let mut ml = MoveList::empty();
+        let expected_move = Move::from_notation("b4", "d5", Either::Left(MoveType::quiet()));
+
+        ml.add_move(
+            NOTATION_TO_INDEX("b4"),
+            NOTATION_TO_INDEX("d5")
+        );
+        
+        assert_eq!(ml.moves.len(), 1);
+        assert!(ml.moves.contains(&expected_move));
+    }
+
+    #[test]
+    fn add_attack_adds_attack() {
+        let mut ml = MoveList::empty();
+        let expected_move = Move::from_notation("b4", "d5", Either::Left(MoveType::ATTACK));
+
+        ml.add_attack(
+            NOTATION_TO_INDEX("b4"),
+            NOTATION_TO_INDEX("d5")
+        );
+        
+        assert_eq!(ml.moves.len(), 1);
+        assert!(ml.moves.contains(&expected_move));
+    }
+    #[test]
+    fn add_check_adds_check() {
+        let mut ml = MoveList::empty();
+        let expected_move = Move::from_notation("b4", "d5", Either::Left(MoveType::CHECK));
+
+        ml.add_check(
+            NOTATION_TO_INDEX("b4"),
+            NOTATION_TO_INDEX("d5")
+        );
+        
+        assert_eq!(ml.moves.len(), 1);
+        assert!(ml.moves.contains(&expected_move));
     }
 }
