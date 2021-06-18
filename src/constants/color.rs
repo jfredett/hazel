d1ad1328d8b3ffcb04b160a9a91f2a1040ae6b99
Joe Fredette <jfredett@gmail.com>
2021-06-18 19:13:55 -0400

    [ply-movegen] impl Color#pawn_direction

--- src/constants/color.rs
+++ src/constants/color.rs
@@ -1,7 +1,18 @@
+use super::Direction;
+
 #[derive(Hash, PartialEq, Eq, Clone, Copy)]
 pub enum Color {
     WHITE = 0,
     BLACK = 1
+}
+
+impl Color {
+    pub fn pawn_direction(self) -> Direction {
+        match self {
+            Color::WHITE => { Direction::N }
+            Color::BLACK => { Direction::S }
+        }
+    }
 }
 
 pub const COLORS : [Color; 2] = [
