2d2f031d744853eeb6f50bac48f66c849b7bc4df
Joe Fredette <jfredett@gmail.com>
2021-06-27 15:51:07 -0400

    [ply-movegen] from/into for Bitboard <-> usize

--- src/bitboard/from_into.rs
+++ src/bitboard/from_into.rs
@@ -2,7 +2,6 @@ use crate::bitboard;
 
 use super::*;
 
-
 impl From<u64> for Bitboard {
     fn from(u: u64) -> Self {
         Bitboard::from(u)
@@ -14,3 +13,18 @@ impl Into<u64> for Bitboard {
         self.0
     }
 }
+
+// NOTE: hazel expects to be run on a 64b machine. If you want to run it on a 32b machine. Don't.
+impl From<usize> for Bitboard {
+    fn from(u: usize) -> Self {
+        Bitboard::from(u as u64)
+    }
+}
+
+// NOTE: hazel expects to be run on a 64b machine. If you want to run it on a 32b machine. Don't.
+impl Into<usize> for Bitboard {
+    fn into(self) -> usize {
+        self.0 as usize
+    }
+}
+
