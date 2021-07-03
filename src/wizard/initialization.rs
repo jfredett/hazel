7776ac625c67a57c358a5c5c7da611427f8aaf4e
Joe Fredette <jfredett@gmail.com>
2021-07-03 14:48:43 -0400

    [wizards] tweaks to initialization and spell
    Still have a performance problem in generating these, they get stuck.
    I think the offset math is the problem, but I don't want to lose this work.

--- src/wizard/initialization.rs
+++ src/wizard/initialization.rs
@@ -1,92 +1,125 @@
-use tracing::{instrument};
+use tracing::{debug, info, instrument, trace};
+use crate::constants::INDEX_TO_NOTATION;
 
 use super::*;
 
 impl Wizard {
     pub fn initialize(&mut self) {
-        self.initialize_bishops();
         self.initialize_rooks();
+        self.initialize_bishops();
     }
     
-    // FIXME: There is probably an efficient way to do these both at once, but I can't be arsed
-    // today.
-    #[instrument]
-    fn initialize_bishops(&mut self) {
-        for i in 0..64 {
+    #[instrument(skip(self))]
+    fn initialize_rooks(&mut self) {
+        info!("Initializing Rooks");
+        let mut loop_count = 0;
+        for (i, spell) in self.rooks.iter_mut().enumerate() {
+            let mut restart_count = 0;
             loop {
-                let spell = self.bishops[i];
-                let mask = NOMINAL_BISHOP_ATTACKS[i];
                 let mut restart = false;
-                let mut entries = vec![];
+                let mut entries: Vec<(usize, bool)> = vec![];
                 
-                for (blockers, attacks) in bishop_block_and_attack_board_for(i, mask) {
-                    let key = spell.key_for(blockers);
+                spell.initialize(ROOK_INDEX_MINS[i]..MAX_SHIFT);
 
-                    match self.table[key] {
+                loop_count += 1;
+                if loop_count % 1000 == 0 {
+                    trace!("current rook square: {}", INDEX_TO_NOTATION[i]);
+                }
+                
+                for (blockers, attacks) in rook_block_and_attack_board_for(i, NOMINAL_ROOK_ATTACKS[i]) {
+                    let key = spell.key_for(blockers);
+                    
+                    let dup = match self.table[key] {
                         Some(attack_board) => { 
                             if attack_board != attacks {
                                 restart = true;
                                 break;
                             }
-                            entries.push((key, true))
-                        }
+                            true
+                        },
                         None => {
                             self.table[key] = Some(attacks);    
-                            entries.push((key, false))
+                            false
                         }
-                    }
+                    };
+                    entries.push((key, dup));
                 }
+                    
                 if restart {
+                    restart_count += 1;
+                    if restart_count % 100 == 0 { 
+                        debug!("rook restarted {}", restart_count); 
+                        trace!("rook init magic: {}", spell.magic);
+                        trace!("rook init shift: {}", spell.shift);
+                        trace!("rook init offset: {}", spell.offset);
+                    }
                     // Clean up everything
                     for (entry, dup) in entries {
                         if dup { continue; }
                         self.table[entry] = None
                     }
-                    self.bishops[i].initialize(5..MAX_SHIFT);
+                    spell.mutate();
                 } else {
                     break;
                 }
+                
             }
         }
     }
-    
-    #[instrument]
-    fn initialize_rooks(&mut self) {
-        for i in 0..64 {
+    #[instrument(skip(self))]
+    fn initialize_bishops(&mut self) {
+        info!("Initializing Bishops");
+        let mut loop_count = 0;
+        for (i, spell) in self.bishops.iter_mut().enumerate() {
+            let mut restart_count = 0;
             loop {
-                let spell = self.rooks[i];
-                let mask = NOMINAL_ROOK_ATTACKS[i];
                 let mut restart = false;
-                let mut entries = vec![];
+                let mut entries: Vec<(usize, bool)> = vec![];
                 
-                
-                for (blockers, attacks) in rook_block_and_attack_board_for(i, mask) {
-                    let key = spell.key_for(blockers);
+                spell.initialize(BISHOP_INDEX_MINS[i]..MAX_SHIFT);
 
-                    match self.table[key as usize] {
+                loop_count += 1;
+                if loop_count % 1000 == 0 {
+                    trace!("current bishop square: {}", INDEX_TO_NOTATION[i]);
+                }
+                
+                for (blockers, attacks) in bishop_block_and_attack_board_for(i, NOMINAL_BISHOP_ATTACKS[i]) {
+                    let key = spell.key_for(blockers);
+                    
+                    let dup = match self.table[key] {
                         Some(attack_board) => { 
                             if attack_board != attacks {
                                 restart = true;
-                                break
+                                break;
                             }
-                            entries.push((key, true))
-                        }
+                            true
+                        },
                         None => {
-                            self.table[key as usize] = Some(attacks);    
-                            entries.push((key, false))
+                            self.table[key] = Some(attacks);    
+                            false
                         }
-                    }
+                    };
+                    entries.push((key, dup));
                 }
-                if !restart {
+                    
+                if restart {
+                    restart_count += 1;
+                    if restart_count % 1000 == 0 { 
+                        debug!("bishop restarted {}", restart_count); 
+                        trace!("bishop init magic: {}", spell.magic);
+                        trace!("bishop init shift: {}", spell.shift);
+                        trace!("bishop init offset: {}", spell.offset);
+                    }
                     // Clean up everything
                     for (entry, dup) in entries {
                         if dup { continue; }
                         self.table[entry] = None
                     }
-                    self.rooks[i].initialize(10..MAX_SHIFT);
+                    spell.mutate();
                 } else {
                     break;
                 }
+
             }
         }
     }
@@ -94,9 +127,12 @@ impl Wizard {
 
 #[cfg(test)]
 mod tests {
+    use tracing_test::traced_test;
+
     use super::*;
     
     #[test]
+    #[traced_test]
     fn an_initialized_wizard_has_slots_filled_in_their_table() {
         let mut w = Wizard::empty();
         w.initialize();
@@ -104,5 +140,32 @@ mod tests {
            if e.is_some() { return } 
         }
         panic!("No items set");
+    }
+    
+
+    #[test]
+    fn quick_bish() {
+        let mut count = 0;
+        for sq in 0..64 {
+            let mask = NOMINAL_BISHOP_ATTACKS[sq];
+            for _ in bishop_block_and_attack_board_for(sq, mask) {
+                count += 1;
+            }
+        }
+        dbg!(count);
+        assert!(false);
+    }
+
+    #[test]
+    fn quick_rook() {
+        let mut count = 0;
+        for sq in 0..64 {
+            let mask = NOMINAL_ROOK_ATTACKS[sq];
+            for _ in rook_block_and_attack_board_for(sq, mask) {
+                count += 1;
+            }
+        }
+        dbg!(count);
+        assert!(false);
     }
 }
--- src/wizard/spell.rs
+++ src/wizard/spell.rs
@@ -1,16 +1,16 @@
-use std::ops::Range;
-use rand::distributions::uniform::SampleRange;
+use std::ops::{BitAndAssign, Range};
+use rand::{distributions::{Standard, uniform::SampleRange}, prelude::Distribution};
 use crate::bitboard::Bitboard;
 use super::*;
-use tracing::debug;
+use tracing::{debug, info, instrument};
 
 // 13b
 #[derive(PartialEq, Eq, Hash, Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct Spell {
-    magic: u64,
-    shift: u8,
-    shift_mask: u64,
-    offset: u32
+    pub(crate) magic: u64,
+    pub(crate) shift: u8,
+    pub(crate) shift_mask: u64,
+    pub(crate) offset: u32
 }
 
 impl Spell {
@@ -23,9 +23,15 @@ impl Spell {
         }
     }
     
+    pub fn new(shift_range: Range<u8>) -> Spell {
+        let mut r = Spell::empty();
+        r.initialize(shift_range);
+        r
+    }
+    
     pub fn initialize(&mut self, shift_range: Range<u8>) {
         let mut rng = rand::thread_rng();
-        self.magic = Spell::low_bit_random_u64();
+        self.magic = Spell::low_bit_random(3);
         // The shift should not allow it to create out-of-bounds indices, and the minimum shift should
         // be `5` (the minimum shift of a bishop).
         // NOTE: Make this range subject to selection?
@@ -35,25 +41,45 @@ impl Spell {
         // The maximum offset should be less than the greater piece-table size. It also needs to fit 
         // within the table itself, which is around ~2meg in size.
         // NOTE: Make the modulus, offset value subject to selection?
-        self.offset = rand::random::<u32>();
+        self.offset = rand::random::<u32>() & self.shift_mask as u32;
     }
     
     pub fn key_for(&self, blockers: Bitboard) -> usize {
         let mut raw_key : u64 = ((blockers * self.magic) >> (64 - self.shift as u64)).into();
         raw_key += self.offset as u64;
-        let ret = (raw_key & self.shift_mask) as usize;
-        if ret > TABLE_SIZE {
-            debug!(ret);
+        (raw_key & self.shift_mask) as usize
+    }
+
+    #[instrument(skip(self))]
+    pub fn mutate(&mut self) {
+        self.magic = Spell::low_bit_random::<u64>(3);
+
+        match rand::random::<u8>() % 4 {
+            0 => { self.shift -= 1; }
+            1 => { self.shift += 1; }
+            _ => {} 
         }
-        ret
+        if self.shift < 5 { self.shift = 5; }
+        if self.shift > MAX_SHIFT { self.shift = MAX_SHIFT; }
+        
+        self.shift_mask = 2u64.pow(self.shift.into()) - 1;
+
+        self.offset ^= Spell::low_bit_random::<u32>(2);
+        self.offset &= self.shift_mask as u32;
     }
 
-    fn low_bit_random_u64() -> u64 {
-        rand::random::<u64>() & rand::random::<u64>() & rand::random::<u64>()
+    /// Produce a low-bit-count random number by generating ANDing-bitwise multiple random numbers together
+    fn low_bit_random<T>(reps: usize) -> T where 
+        T : BitAndAssign,
+        Standard : Distribution<T> {
+            let mut r = rand::random::<T>();
+            for _ in 0..reps {
+                r &= rand::random::<T>();
+            }
+            r
     }
+    
 }
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -67,5 +93,32 @@ mod tests {
         let serialized = bincode::serialize(&expected).unwrap();
         let deserialized : Spell = bincode::deserialize(&serialized).unwrap();
         assert_eq!(deserialized, expected);
+    }
+    
+    #[test]
+    fn mutation_changes_values() {
+        let original = Spell::new(5..18); 
+        let mut mutated = original;
+        
+        assert_eq!(original, mutated);
+
+        mutated.mutate();
+
+        assert_ne!(original, mutated);
+    }
+    
+    // This test expects a panic if it's broken.
+    #[quickcheck]
+    fn key_for_does_not_violate_table_boundaries(blockers: Bitboard) {
+        let s = Spell::new(5..18);
+        s.key_for(blockers);
+    }
+    
+    // This test expects a panic if it's broken.
+    #[quickcheck]
+    fn key_for_does_not_violate_table_boundaries_even_after_mutation(blockers: Bitboard) {
+        let mut s = Spell::new(5..18);
+        s.mutate();
+        s.key_for(blockers);
     }
 }
