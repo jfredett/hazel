287d68e47e1a8183798886f8330e014ce3fd01d5
Joe Fredette <jfredett@gmail.com>
2021-07-09 16:48:03 -0400

    [pext] benchmarks for pextboard vs slow method

--- benches/movegen/mod.rs
+++ benches/movegen/mod.rs
@@ -1,7 +1,6 @@
+use super::*;
 
-
-/*
-use hazel::constants::move_tables::ROOK_ATTACKS;
+use hazel::pextboard::*;
 
 bench!(
     group: MoveGen,
@@ -9,17 +8,36 @@ bench!(
     name: rook_attacks_slow,
     test: { slow_rook_attacks(rook_idx, occupancy) },
     where:
-        rook_idx => Bitboard::from(random_u64() % 64); 
+        rook_idx => Bitboard::from(1 << (random_u64() % 64)); 
         occupancy => random_bitboard();
 );
 
 bench!(
     group: MoveGen,
-    pretty: "rook_attacks/2 - Plain Magic Method",
+    pretty: "rook_attacks/2 - PEXTBoard Method",
     name: rook_attacks_slow,
-    test: { ROOK_ATTACKS[rook_idx as usize].attacks_for(occupancy) },
+    test: { hazel::pextboard::attacks_for(Piece::Rook, rook_idx, occupancy) },
     where:
-        rook_idx => random_u64() % 64; 
+        rook_idx => random_usize() % 64; 
         occupancy => random_bitboard();
 );
-*/
+
+bench!(
+    group: MoveGen,
+    pretty: "bishop_attacks/2 - Slow Method",
+    name: bishop_attacks_slow,
+    test: { slow_bishop_attacks(bishop_idx, occupancy) },
+    where:
+        bishop_idx => Bitboard::from(1 << (random_u64() % 64)); 
+        occupancy => random_bitboard();
+);
+
+bench!(
+    group: MoveGen,
+    pretty: "bishop_attacks/2 - PEXTBoard Method",
+    name: bishop_attacks_slow,
+    test: { hazel::pextboard::attacks_for(Piece::Bishop, bishop_idx, occupancy) },
+    where:
+        bishop_idx => random_usize() % 64; 
+        occupancy => random_bitboard();
+);
