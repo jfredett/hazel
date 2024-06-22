5989e77af0e900a8695be0bd4dea273bae01a1e0
Joe Fredette <jfredett@gmail.com>
2024-06-21 20:52:39 -0400

    [chore] add bugstalker to flake
    testing it out

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     fenix.url = "github:nix-community/fenix";
     devenv.url = "github:cachix/devenv";
+    bugstalker.url = "github:godzie44/BugStalker";
   };
 
-  outputs = { self, nixpkgs, devenv, fenix, ... } @ inputs:
+  outputs = { self, nixpkgs, bugstalker, devenv, fenix, ... } @ inputs:
   let
     systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
     forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
@@ -17,6 +18,7 @@
     in {
       default = devenv.lib.mkShell {
         inherit inputs pkgs;
+
         modules = [{
           languages.rust.enable = true;
           languages.rust.channel = "nightly";
@@ -27,6 +29,7 @@
 
           packages = with pkgs; [ 
             gnuplot 
+            bugstalker.packages."x86_64-linux".default
             perf-tools
             linuxKernel.packages.linux_6_6.perf
           ];
