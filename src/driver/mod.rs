a6eb44446b08252fc242a9085bd82ce94036f4f2
Joe Fredette <jfredett@gmail.com>
2024-07-03 22:28:46 -0400

    Rough in all UCI Messages, implement Debug

--- src/driver/mod.rs
+++ src/driver/mod.rs
@@ -9,26 +9,74 @@ use crate::uci::UCIMessage;
 use crate::game::Game;
 
 
-struct Driver {
+pub struct Driver {
+    debug: bool
 }
 
 impl Driver {
 
     pub fn new() -> Self {
-        Driver {}
+        Driver {
+            debug: false
+        }
     }
 
-    pub fn exec(&mut self, message: UCIMessage) -> UCIMessage {
+    pub fn exec(&mut self, message: UCIMessage) -> Option<UCIMessage> {
         match message {
+            // GUI -> Engine
             UCIMessage::IsReady => {
                 info!("Received IsReady");
-                UCIMessage::ReadyOk
+                Some(UCIMessage::ReadyOk)
             }
             UCIMessage::UCI => {
                 info!("Received UCI");
-                UCIMessage::ID("Hazel".to_string(), "0.1".to_string())
+                Some(UCIMessage::ID("Hazel".to_string(), "0.1".to_string()))
             }
-
+            UCIMessage::Debug(flag) => {
+                info!("Received Debug");
+                self.debug = flag;
+                None
+            }
+            UCIMessage::SetOption(name, values) => {
+                info!("Received SetOption");
+                None
+            }
+            UCIMessage::Register => {
+                info!("Received Register");
+                None
+            }
+            UCIMessage::UCINewGame => {
+                info!("Received UCINewGame");
+                None
+            }
+            UCIMessage::Position(_, _) => {
+                info!("Received Position");
+                None
+            }
+            UCIMessage::Go(_) => {
+                info!("Received Go");
+                None
+            }
+            UCIMessage::Stop => {
+                info!("Received Stop");
+                None
+            }
+            UCIMessage::PonderHit => {
+                info!("Received PonderHit");
+                None
+            }
+            UCIMessage::Quit => {
+                info!("Received Quit");
+                None
+            }
+            // Engine -> GUI
+            UCIMessage::ID(_,_) => None,
+            UCIMessage::ReadyOk => None,
+            UCIMessage::BestMove(_, _) => None,
+            UCIMessage::CopyProtection => None,
+            UCIMessage::Registration => None,
+            UCIMessage::Info(_) => None,
+            UCIMessage::Option(_, _) => None,
             _ => {
                 error!("Unexpected message: {:?}", message);
                 panic!("Unexpected message");
@@ -48,7 +96,7 @@ mod tests {
     fn driver_parses_isready() {
         let mut driver = Driver::new();
         let response = driver.exec(UCIMessage::IsReady);
-        assert_eq!(response, UCIMessage::ReadyOk);
+        assert_eq!(response, Some(UCIMessage::ReadyOk));
     }
 
     #[traced_test]
@@ -56,9 +104,16 @@ mod tests {
     fn driver_parses_uci() {
         let mut driver = Driver::new();
         let response = driver.exec(UCIMessage::UCI);
-        assert_eq!(response, UCIMessage::ID("Hazel".to_string(), "0.1".to_string()));
+        assert_eq!(response, Some(UCIMessage::ID("Hazel".to_string(), "0.1".to_string())));
     }
 
-
-
+    #[traced_test]
+    #[test]
+    fn driver_parses_debug() {
+        let mut driver = Driver::new();
+        assert!(!driver.debug);
+        let response = driver.exec(UCIMessage::Debug(true));
+        assert_eq!(response, None);
+        assert!(driver.debug)
+    }
 }
