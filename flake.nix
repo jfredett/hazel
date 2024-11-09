1927d4b05645b4b944caf09e826ce590b3e5e073
Joe Fredette <jfredett@gmail.com>
2024-11-09 00:11:13 -0500

    cargo-llvm-cov isn't compiling on unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     fenix.url = "github:nix-community/fenix";
     devenv.url = "github:cachix/devenv";
     bugstalker.url = "github:godzie44/BugStalker";
