efa5293a9babef70380316d9da158ad1e38655d3
Joe Fredette <jfredett@gmail.com>
2024-11-11 15:42:52 -0500

    parse halfply number markers in pgn

--- src/notation/pgn/mod.rs
+++ src/notation/pgn/mod.rs
@@ -1,9 +1,12 @@
 use std::path::Path;
 use std::io::Error;
 
-use nom::{character::complete::{alpha1, char, newline, none_of, space1}, multi::{many0, many1}, sequence::delimited, IResult};
+use nom::{branch::alt, bytes::complete::{tag, take_while}, character::{complete::{alpha1, char, multispace0, multispace1, newline, none_of, one_of, space1}, is_digit}, combinator::{not, opt}, multi::{many0, many1}, sequence::delimited, IResult};
+use tracing::debug;
 
-use crate::game::variation::Variation;
+use crate::{constants::File, coup::rep::Move, game::variation::Variation, types::Piece};
+
+use super::Square;
 
 #[derive(Debug)]
 pub struct PGN {
@@ -57,6 +60,24 @@ impl TagPair {
     }
 }
 
+impl Variation {
+    /// Given a PGN ply notation like:
+    ///
+    /// XX. YY ZZ
+    ///
+    /// This returns a result of ("YY ZZ", XX as usize)
+    ///
+    /// Note that it consumes _all available whitespace_ after the ply number, and optionally will
+    /// read the period after the ply number if it is present.
+    pub fn half_ply_number(input: &str) -> IResult<&str, usize> {
+        let (input, half_ply_num) = delimited(multispace0, many1(one_of("1234567890")), alt((tag("."), multispace1)))(input)?;
+
+        // remove any excess whitespace because I'm not lexing.
+        let (input, _) = multispace0(input)?;
+        let half_ply = half_ply_num.iter().collect::<String>().parse::<usize>().unwrap();
+        Ok((input, half_ply))
+    }
+}
 /*
 impl<S : Into<String>> TryFrom<S> for PGN {
 type Error = Error;
@@ -94,6 +115,58 @@ impl PGN {
 mod tests {
     use super::*;
 
+    mod halfply_marker {
+        use crate::types::Color;
+
+        use super::*;
+
+
+        #[test]
+        fn parses_halfply_marker() {
+            let hp = "1. d4 d5";
+            let (input, half_ply) = Variation::half_ply_number(hp).unwrap();
+
+            assert_eq!(input, "d4 d5");
+            assert_eq!(half_ply, 1);
+        }
+
+        #[test]
+        fn parses_halfply_marker_with_multiple_digits() {
+            let hp = "30. d4 d5";
+            let (input, half_ply) = Variation::half_ply_number(hp).unwrap();
+
+            assert_eq!(input, "d4 d5");
+            assert_eq!(half_ply, 30);
+        }
+
+        #[test]
+        fn parses_halfply_missing_period() {
+            let hp = "30 d4 d5";
+            let (input, half_ply) = Variation::half_ply_number(hp).unwrap();
+
+            assert_eq!(input, "d4 d5");
+            assert_eq!(half_ply, 30);
+        }
+
+
+        #[quickcheck]
+        fn parses_halfply_numbers_correctly(half_ply: usize, full_move: bool, include_period: bool) -> bool {
+            if half_ply == 0 { return true; }
+            let period = if include_period { "." } else { "" };
+
+            let hp = if full_move {
+                format!("{}{} x4 y8", half_ply, period)
+            } else {
+                format!("{}{} z1", half_ply, period)
+            };
+
+            let (input, parsed_half_ply) = Variation::half_ply_number(&hp).unwrap();
+
+            let input_check = if full_move { input == "x4 y8" } else { input == "z1" };
+            input_check && (half_ply == parsed_half_ply)
+        }
+
+    }
     mod tagpairs {
         use tracing::debug;
 
