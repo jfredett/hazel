f5d31c082ef511261ce9ec2f9f253337669aeaf9
Joe Fredette <jfredett@gmail.com>
2025-01-11 14:41:57 -0500

    note on testing run_with_io

--- src/engine/uci/connection.rs
+++ src/engine/uci/connection.rs
@@ -1,8 +1,7 @@
-
-
 // Open a (provided) communication stream, read it by line, and parse it into UCI messages.
 
-use std::io::{self, BufRead};
+use std::io;
+use std::io::BufRead;
 
 use tracing::{error, info};
 
@@ -10,7 +9,7 @@ use crate::engine::uci::UCIMessage;
 use crate::engine::driver::{HazelResponse, WitchHazel};
 
 #[cfg_attr(test, mutants::skip)]
-pub async fn run() -> io::Result<()> {
+pub async fn run() -> tokio::io::Result<()> {
     run_with_io(io::stdin(), io::stdout()).await
 }
 
@@ -20,8 +19,10 @@ pub const BUF_SIZE: usize = 1024;
 
 // take arbitrary IO Streams and use it as if it were STDIN/STDOUT
 // to do the `run` function above
-pub async fn run_with_io<T,U>(input: T, mut output: U) -> io::Result<()> 
-where T: 'static + io::Read + Send, U: 'static + io::Write + Send {
+pub async fn run_with_io<T, U>(input: T, mut output: U) -> tokio::io::Result<()>
+where T: 'static + io::Read + Send + Unpin,
+      U: 'static + io::Write + Send + Unpin
+{
     let hazel = WitchHazel::<1024>::new().await;
 
     let echo_handle = hazel.clone();
@@ -35,18 +36,33 @@ where T: 'static + io::Read + Send, U: 'static + io::Write + Send {
                 Ok(_) => {},
                 Err(e) => { error!("Error writing to output: {}", e); }
             }
-            output.flush().unwrap();
         }
     });
 
-    let input = io::BufReader::new(input);
-
+    let mut input = io::BufReader::new(input);
     info!("Starting input task");
-    let handle = io::BufReader::new(input);
-    let mut lines = handle.lines();
     loop {
-        let line = lines.next().unwrap().unwrap();
+        let mut line = String::new();
+        _ = input.read_line(&mut line);
         let message = UCIMessage::parse(&line);
         hazel.send(Box::new(message)).await;
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// this tests the run_with_io function using a simulated STDIN/STDOUT and verifies the
+    /// `isready` command will recieve a response of `readyok`
+    #[tokio::test]
+    async fn test_run_with_io() {
+        // TODO: I have no idea how to test this with the current design. Trying to get a stream
+        // that I can see from two places turns out to be not straightforward.
+        //
+        // I'm pretty sure this all works, but it should be tested. I think the SyncIOBridge type
+        // might be useful here, but I need to do some more reading on it. For now, this works by
+        // inspection and I think I'm just going to eat the coverage loss.
     }
 }
