d6cb2dbad31f103f60c319431e95ed34bf2f6a05
Joe Fredette <jfredett@gmail.com>
2025-02-17 15:58:43 -0500

    rename hazelinitialization -> initialization

--- src/engine/driver/hazel/messages/initialization.rs
+++ src/engine/driver/hazel/messages/initialization.rs
@@ -1,14 +1,13 @@
 use async_trait::async_trait;
-use tracing::error;
 
 use crate::{engine::driver::{Hazel, HazelResponse, State}, types::witch::{MessageFor, Witch}};
 
-pub struct HazelInitialization; /* {
+pub struct Initialization; /* {
     // this is where config file loading can go?
 } */
 
 #[async_trait]
-impl<const BUF_SIZE: usize> MessageFor<Witch<BUF_SIZE, Hazel, HazelResponse>> for HazelInitialization {
+impl<const BUF_SIZE: usize> MessageFor<Witch<BUF_SIZE, Hazel, HazelResponse>> for Initialization {
     async fn run(&self, witch: &mut Witch<BUF_SIZE, Hazel, HazelResponse>) {
         // witch.write(HazelResponse::Transition(State::Ready));
         witch.state.state = State::Ready;
