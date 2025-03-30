1e2f9e412d32339622925a4923d5d5911132e8f2
Joe Fredette <jfredett@gmail.com>
2025-03-30 15:12:04 -0400

    create test crate for integration tests

---
+++ tests/pieceboard-query_test.rs
@@ -0,0 +1,74 @@
+use hazel::board::simple::PieceBoard;
+use hazel::constants::POS2_KIWIPETE_FEN;
+use hazel::extensions::query::display_board;
+use hazel_basic::square::*;
+use hazel_basic::interface::Query;
+
+#[test]
+fn display_test() {
+    let mut p = PieceBoard::default();
+    p.set_startpos();
+
+    let actual = display_board(&p);
+    let expected = "8 r n b q k b n r
+7 p p p p p p p p
+6 . . . . . . . .
+5 . . . . . . . .
+4 . . . . . . . .
+3 . . . . . . . .
+2 P P P P P P P P
+1 R N B Q K B N R
+  a b c d e f g h
+";
+
+    println!("{}", actual);
+    println!("{}", expected);
+    assert_eq!(actual, expected);
+}
+
+mod to_fen_position {
+
+    use hazel_basic::ben::BEN;
+
+    use super::*;
+
+    #[test]
+    fn to_fen_test() {
+        let mut p = PieceBoard::default();
+        p.set_startpos();
+
+        let actual = hazel_basic::interface::query::to_fen_position(&p);
+        let expected = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+
+        assert_eq!(format!("{}", actual), expected);
+    }
+
+    #[test]
+    fn to_fen_test_kiwipete() {
+        let mut p = PieceBoard::default();
+        p.set_fen(BEN::new(POS2_KIWIPETE_FEN));
+
+        let actual = hazel_basic::interface::query::to_fen_position(&p);
+        let expected = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R";
+
+        assert_eq!(format!("{}", actual), expected);
+    }
+
+    #[test]
+    fn is_empty() {
+        let mut p = PieceBoard::default();
+        p.set_startpos();
+
+        assert!(p.is_empty(A3));
+        assert!(!p.is_empty(A2));
+    }
+
+    #[test]
+    fn is_occupied() {
+        let mut p = PieceBoard::default();
+        p.set_startpos();
+
+        assert!(!p.is_occupied(A3));
+        assert!(p.is_occupied(A2));
+    }
+}
