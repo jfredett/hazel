24972acac3034b78336e8915d6daa54840612eba
Joe Fredette <jfredett@gmail.com>
2025-04-01 10:43:18 -0400

    update tui-logger, rand crates

--- hooks/pextboard/src/main.rs
+++ hooks/pextboard/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     let mut v = [Bitboard::empty(); 10];
     for i in 0..COUNT {
         let bb_in = rand::random::<u64>();
-        let sq = Square::new(rand::random::<usize>() % 64);
+        let sq_idx = rand::random::<u64>() % 64;
+        let sq = Square::new(sq_idx.try_into().unwrap());
         let blocks: Bitboard = Bitboard::new(bb_in);
 
         v[i % 10] = pextboard::attacks_for(Piece::Rook, sq, blocks);
